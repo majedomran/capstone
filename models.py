664a2a20dbd60834999bd0240e84b963b58e7d32
Majed <majedbinomran@gmail.com>
2020-10-31 00:00:47 +0300

    updating bugs in database

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.drob_allO()
     db.create_all()
 
 '''
